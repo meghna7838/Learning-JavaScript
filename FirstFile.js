97a2589ccb0c5e3dfba21d4715670cecb7136ba1
meghna7838 <meghnatandon7838@gmail.com>
2024-01-01 20:59:03 +0530

    Practise Switch

--- FirstFile.js
+++ FirstFile.js
@@ -251,6 +251,28 @@ num%2 ==0?
 console.log("Even"):
 console.log("Odd");
 
+let amount =1500;
+switch(true){
+    case (amount<500):
+        discount="No Discount";
+        break;
+    case (amount>=500 && amount<1000):
+        discount="10% Discount";
+        break;
+    case (amount>=1000 && amount<2000):
+        discount="20% Discount";
+        break;
+    case (amount>=2000 && amount<4000):
+        discount="30% Discount";
+        break;
+    case (amount>=4000 && amount<5000):
+        discount="40% Discount";
+        break;
+    default:
+        discount="50% Discount";  
+}
+
+
 
 
 
