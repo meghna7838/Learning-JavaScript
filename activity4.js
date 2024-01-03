15af157744b0f128fad1b2c66808f5ccb4791327
meghna7838 <meghnatandon7838@gmail.com>
2024-01-03 23:34:08 +0530

    Accessing keys and values

--- activity4.js
+++ activity4.js
@@ -1,70 +1,70 @@
 //Complete the findClassTopper function
 //Do not alter alter the starter code. 
     //The object on which you will be working will be of this format.
-    const studentMarks = {
-        John: [85, 90, 92, 88, 87],
-        Jane: [92, 95, 89, 91, 94],
-        David: [78, 85, 90, 92, 84],
-        Emily: [90, 88, 92, 87, 91],
-        Michael: [86, 92, 90, 89, 94]
-        };
-        function findClassTopper(studentMarks){
-            //Implement your function here
-            let topper="";
-            let max=0;
-            let marksJohn=0;
-            let marksJane=0;
-            let marksDavid=0;
-            let marksEmily=0;
-            let marksMichael=0;
-            for(let i=0;i<5;i++)
-                {
-             marksJohn += studentMarks.John[i];
-                }
+    // const studentMarks = {
+    //     John: [85, 90, 92, 88, 87],
+    //     Jane: [92, 95, 89, 91, 94],
+    //     David: [78, 85, 90, 92, 84],
+    //     Emily: [90, 88, 92, 87, 91],
+    //     Michael: [86, 92, 90, 89, 94]
+    //     };
+    //     function findClassTopper(studentMarks){
+    //         //Implement your function here
+    //         let topper="";
+    //         let max=0;
+    //         let marksJohn=0;
+    //         let marksJane=0;
+    //         let marksDavid=0;
+    //         let marksEmily=0;
+    //         let marksMichael=0;
+    //         for(let i=0;i<5;i++)
+    //             {
+    //          marksJohn += studentMarks.John[i];
+    //             }
            
-                if (max<marksJohn) 
-                {
-                max=marksJohn;
-                topper="John";
-                }
-            for(let i=0;i<5;i++)
-                {
-             marksJane += studentMarks.Jane[i];
-                }
-                if (max<marksJane)
-                {   
-                max=marksJane;
-                topper="Jane";
-                }
-            for(let i=0;i<5;i++)
-             {
-             marksDavid += studentMarks.David[i];
-            }
-            if (max<marksDavid)
-            {   
-            max=marksDavid;
-            topper="David";
-            }
+    //             if (max<marksJohn) 
+    //             {
+    //             max=marksJohn;
+    //             topper="John";
+    //             }
+    //         for(let i=0;i<5;i++)
+    //             {
+    //          marksJane += studentMarks.Jane[i];
+    //             }
+    //             if (max<marksJane)
+    //             {   
+    //             max=marksJane;
+    //             topper="Jane";
+    //             }
+    //         for(let i=0;i<5;i++)
+    //          {
+    //          marksDavid += studentMarks.David[i];
+    //         }
+    //         if (max<marksDavid)
+    //         {   
+    //         max=marksDavid;
+    //         topper="David";
+    //         }
            
-            for(let i=0;i<5;i++)
-                {
-             marksEmily += studentMarks.Emily[i];
-                }
-            if (max<marksEmily) {max=marksEmily;
-             topper="Emily";
-            }
-            for(let i=0;i<5;i++)
-                {
-             marksMichael += studentMarks.Michael[i];
-                }
-            if (max<marksMichael) {max=marksMichael;
-             topper="Michael";
-            }
-            return topper;
+    //         for(let i=0;i<5;i++)
+    //             {
+    //          marksEmily += studentMarks.Emily[i];
+    //             }
+    //         if (max<marksEmily) {max=marksEmily;
+    //          topper="Emily";
+    //         }
+    //         for(let i=0;i<5;i++)
+    //             {
+    //          marksMichael += studentMarks.Michael[i];
+    //             }
+    //         if (max<marksMichael) {max=marksMichael;
+    //          topper="Michael";
+    //         }
+    //         return topper;
             
-        }
-        console.log(findClassTopper(studentMarks));
-        //Output : "Jane";
+    //     }
+    //     console.log(findClassTopper(studentMarks));
+    //     //Output : "Jane";
         
 
         //Right way
@@ -98,8 +98,31 @@
             }
             return classTopper;
           }
-        
 
+          //Complete the URLconstructor function 
+// Do not change the starter code. 
+
+//Complete the URLconstructor function 
+// Do not change the starter code. 
+
+function URLconstructor(queryParameters,domain,path){
+    let finalurl = "";
+    finalurl += "https://";
+    finalurl += domain;
+    finalurl += path;
+    for( let key in queryParameters){
+        finalurl += '?';
+        finalurl += key;
+        finalurl += '=';
+        finalurl += queryParameters[key];
+    }
+    return finalurl; 
+}
+const queryParameters = {name:'John',age:'28'}; 
+const domain = "google.com";
+const path = '/search';
+console.log(URLconstructor(queryParameters,domain,path));
+//output: https://google.com/search?name=John?age=28
 
 //complete the calculatePrice function
 //Do not alter the starter code
