b6be4462f73422845acb5bd3b1418b7beba99a88
meghna7838 <meghnatandon7838@gmail.com>
2024-01-05 18:11:28 +0530

    playing with scope

--- activity4.js
+++ activity4.js
@@ -249,3 +249,13 @@ console.log(a, b, c);
 var x=10;
 let x=20;
 console.log(x);
+
+function outer(){
+    let x=10;
+    if(true){
+        let y=20;
+        var z=30;
+        console.log(x+y+z);
+    }
+    //console.log(x+y+z);//Error
+}
