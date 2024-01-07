90326ae52b82aa1bc6c5ab2369dea48e730f0619
meghna7838 <meghnatandon7838@gmail.com>
2024-01-07 22:51:13 +0530

    More on function

--- activity5.js
+++ activity5.js
@@ -7,11 +7,14 @@ function sum(a,b)
 }
 const sum1 =sum(6,5);
 console.log(sum1);//11
+//The above lines of code can be moved to line 4 and still we will get same output with no error. 
+//This is because of hoisting 
 
 //Function expression :- function can be assigned to variabes. These are also called anonymous functions as they are not named or used anywhere else
-const sum2 = function(a,b)
+var sum2 = function(a,b)
 {
 return a+b;
 }
 console.log(sum2);// output will come out to be function definition
 console.group(sum2(2,3));//5
+//If we move the above lines to line 13 then we will get error because we are 
