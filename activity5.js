98d3b90b791d8e7cd2eb65347557de2c17d65f18
meghna7838 <meghnatandon7838@gmail.com>
2024-01-07 21:48:07 +0530

    More on function

--- activity5.js
+++ activity5.js
@@ -6,7 +6,7 @@ function sum(a,b)
     return a+b;
 }
 const sum1 =sum(6,5);
-console.log(sum1);
+console.log(sum1);//11
 
 //Function expression :- function can be assigned to variabes. These are also called anonymous functions as they are not named or used anywhere else
 const sum2 = function(a,b)
@@ -14,3 +14,4 @@ const sum2 = function(a,b)
 return a+b;
 }
 console.log(sum2);// output will come out to be function definition
+console.group(sum2(2,3));//5
