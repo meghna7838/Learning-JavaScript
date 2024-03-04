7a6ee8e1a005f77ac302517b0938f6c10609c31b
meghna7838 <meghnatandon7838@gmail.com>
2024-03-04 11:14:59 +0530

    older changes

--- activity6.js
+++ activity6.js
@@ -114,9 +114,7 @@
             acc[key] = [];
           }
 
-          let newObj= {
-            key: [ obj.name]
-          };
+       
           acc[key].push(obj);
           return acc;
         }, {})
@@ -144,3 +142,47 @@
         }, {});
   
       }
+
+     //Complete the totalorderValue function
+// Do not alter the starter code
+    const orders = [
+        {
+          orderNumber: 1,
+          items: [
+            { name: "Cappuccino", price: 3.5 },
+            { name: "Chocolate Croissant", price: 2.5 }
+          ]
+        },
+        {
+          orderNumber: 2,
+          items: [
+            { name: "Latte", price: 4.0 },
+            { name: "Blueberry Muffin", price: 2.75 }
+          ],
+          discountCode: "COFFEELOVER"
+        }
+      ];
+      function totalOrderValue(orders, applyDiscount){
+        //Implement your function here
+      //The function should return the total price after applying the discount.
+        const totalValue=orders.map((order)=>
+                                    {
+            let orderValue  = order.items.reduce((acc,val)=>acc+val.price,0);
+          const discountValue = applyDiscount(order.discountCode,orderValue);
+          return discountValue;
+                                    }
+                                   );
+      }
+      //define your callback function here to calculate the discount.
+     function applyDiscount(discountCode,totalValue)
+  {
+      if(discountCode === "COFFEELOVER")
+          {
+              totalValue = 0.9*totalValue;
+          }else if(discountCode === "TEALOVER")
+              {
+                  totalValue =0.8*totalValue;
+              }
+      return totalValue;
+  }
+    
